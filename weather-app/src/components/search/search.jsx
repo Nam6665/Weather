3de0c3a2f586637d847e6fcdfca127276322b9ee
Nam6665 <37721120+Nam6665@users.noreply.github.com>
2025-03-20 14:45:52 +0100

    commit

---
+++ weather-app/src/components/search/search.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoUrl, geoApiOptions } from "../../api.js";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState("");
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${geoUrl}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
