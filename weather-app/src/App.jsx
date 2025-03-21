9c6747f4ae8d33331c36e28f901027f80c693105
Nam6665 <how80082@gmail.com>
2025-03-21 11:19:01 +0100

    Last coomit

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Search from "./components/search/search.jsx";
 import CurrentWeather from "./components/current-weather/current-weather.jsx";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { getCurrentWeather, getForecast } from "./components/Services/api.js";
 import Forecast from "./components/forecast/forecast.jsx";
+import "./App.css";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -12,51 +13,51 @@ function App() {
     const stockholmLat = 59.3293;
     const stockholmLon = 18.0686;
 
-    fetch(
-      `${WEATHER_API_URL}/weather?lat=${stockholmLat}&lon=${stockholmLon}&appid=${WEATHER_API_KEY}&units=metric`
-    )
-      .then((response) => response.json())
-      .then((weatherResponse) => {
+    Promise.all([
+      getCurrentWeather(stockholmLat, stockholmLon),
+      getForecast(stockholmLat, stockholmLon),
+    ])
+      .then(([weatherResponse, forecastResponse]) => {
         setCurrentWeather({ city: "Stockholm", ...weatherResponse });
-      })
-      .catch((err) => console.log(err));
-
-    fetch(
-      `${WEATHER_API_URL}/forecast?lat=${stockholmLat}&lon=${stockholmLon}&appid=${WEATHER_API_KEY}&units=metric`
-    )
-      .then((response) => response.json())
-      .then((forecastResponse) => {
         setForecast({ city: "Stockholm", ...forecastResponse });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        alert("Unable to fetch weather data. Please try again later.");
+      });
   }, []);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
-
+    Promise.all([getCurrentWeather(lat, lon), getForecast(lat, lon)])
+      .then(([weatherResponse, forecastResponse]) => {
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecast({ city: searchData.label, ...forecastResponse });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        alert("Unable to fetch weather data. Please try again later.");
+      });
   };
 
   return (
     <div className='container'>
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
+      <div className='search-container'>
+        <Search onSearchChange={handleOnSearchChange} />
+      </div>
+      <div className='weather-forecast-container'>
+        <div className='current-weather'>
+          {currentWeather ? (
+            <CurrentWeather data={currentWeather} />
+          ) : (
+            <p>Loading current weather...</p>
+          )}
+        </div>
+        <div className='forecast-container'>
+          {forecast ? <Forecast data={forecast} /> : <p>Loading forecast...</p>}
+        </div>
+      </div>
     </div>
   );
 }
